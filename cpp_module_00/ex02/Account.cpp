d3194645af7788de8a77ad827fefe163ea6e4ce0
eulrho <sunriselss19@gmail.com>
2024-05-28 14:49:07 +0900

    [Fix] time object value

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -41,7 +41,7 @@ void	Account::_displayTimestamp( void )
 	std::cout << t->tm_year + 1900;
 	if (t->tm_mon < 10)
 		std::cout << "0";
-	std::cout << t->tm_mon;
+	std::cout << t->tm_mon + 1;
 	if (t->tm_mday < 10)
 		std::cout << "0";
 	std::cout << t->tm_mday << "_";
