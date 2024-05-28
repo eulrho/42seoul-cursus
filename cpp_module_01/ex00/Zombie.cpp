49570eed2a872f6881e683c7563e7f6b34ea87b8
eulrho <sunriselss19@gmail.com>
2024-05-28 16:59:00 +0900

    [Fix] delete setter

--- cpp_module_01/ex00/Zombie.cpp
+++ cpp_module_01/ex00/Zombie.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <string>
 #include "Zombie.hpp"
 
-Zombie::Zombie() {}
+Zombie::Zombie(std::string name) : name(name)
+{
+	//std::cout << this->name << " created\n";
+}
 
 Zombie::~Zombie(void)
 {
-	std::cout << this->name << " : I'll be back....\n";
+	//std::cout << this->name << " : I'll be back....\n";
 }
 
 void Zombie::announce(void)
 {
 	std::cout << this->name << " : " << "BraiiiiiiinnnzzzZ...\n";
 }
-
-void Zombie::setName(std::string name) {this->name = name;}
--- cpp_module_01/ex00/Zombie.hpp
+++ cpp_module_01/ex00/Zombie.hpp
@@ -7,10 +7,9 @@ class Zombie {
 	private :
 		std::string name;
 	public :
-		Zombie();
+		Zombie(std::string);
 		~Zombie(void);
 		void announce(void);
-		void setName(std::string);
 };
 
 #endif
