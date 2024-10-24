07f22e6034b3bebc6d2661996614f3165ea7e66e
eulrho <sunriselss19@gmail.com>
2024-10-25 00:27:20 +0900

    [Fix] mariadb setting error

---
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+service mariadb start
+
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
+
+# # Import database dump
+# mariadb -u root $MYSQL_DATABASE < /usr/local/bin/dump.sql
+
+# Set password for root user
+mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+mariadb -u root -e "FLUSH PRIVILEGES;"
+
+# Allow root user to login from any host
+mariadb -u root -p $MYSQL_ROOT_PASSWORD "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -u root -p $MYSQL_ROOT_PASSWORD "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+exec "$@"
