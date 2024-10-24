bcb7a2e73852e2f6f7f334d9adb5606ecb9a442b
eulrho <sunriselss19@gmail.com>
2024-10-25 04:48:28 +0900

    [Fix] config file setting error

--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget -O ./wordpress.tar.gz http://wordpress.org/latest.tar.gz
-tar -xvf wordpress.tar.gz
+wget http://wordpress.org/latest.tar.gz
 mkdir -p /var/www/html/
-mv wordpress/* /var/www/html/
-rm -rf wordpress.tar.gz
+tar -xvf latest.tar.gz
+mv wordpress /var/www/html
+rm -rf latest.tar.gz
 
 chown -R www-data:www-data /var/www/html/
 
@@ -12,9 +12,10 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -
 chmod +x /usr/local/bin/wp
 
 mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
-wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html
-wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
-wp config set DB_HOST mariadb --allow-root --path=/var/www/html
+
+wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/wordpress
+wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/wordpress
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/wordpress
+wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html/wordpress
 
 exec "$@"
