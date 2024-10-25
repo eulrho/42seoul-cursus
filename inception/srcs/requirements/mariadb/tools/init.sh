c04960590f625ba5d9b5e2e4173b53f4eea38fc0
eulrho <sunriselss19@gmail.com>
2024-10-25 11:50:48 +0900

    tmp

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -2,6 +2,8 @@
 
 service mariadb start
 
+sleep 2
+
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
@@ -11,12 +13,12 @@ mariadb -u root -e "FLUSH PRIVILEGES;"
 # mariadb -u root $MYSQL_DATABASE < /usr/local/bin/dump.sql
 
 # Set password for root user
-mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+mariadb -u root -e "SET PASSWORD FOR 'root'@'%' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
 # Allow root user to login from any host
-mariadb -u root -p $MYSQL_ROOT_PASSWORD "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mariadb -u root -p $MYSQL_ROOT_PASSWORD "FLUSH PRIVILEGES;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" "FLUSH PRIVILEGES;"
 
 service mariadb stop
 
--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -3,7 +3,7 @@
 wget http://wordpress.org/latest.tar.gz
 mkdir -p /var/www/html/
 tar -xvf latest.tar.gz
-mv wordpress /var/www/html
+mv wordpress/* /var/www/html
 rm -rf latest.tar.gz
 
 chown -R www-data:www-data /var/www/html/
@@ -11,11 +11,11 @@ chown -R www-data:www-data /var/www/html/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/wordpress
-wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/wordpress
-wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/wordpress
-wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html/wordpress
+wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/
+wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/
+wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html/
 
 exec "$@"
