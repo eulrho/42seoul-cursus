86f9d607a4efe802e0b4225691137620b329e259
eulrho <sunriselss19@gmail.com>
2024-10-25 10:55:13 +0900

    [Feat] add nginx

---
+++ inception/srcs/requirements/nginx/tools/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout ${CERTS_KEY} -out ${CERTS_CRT} -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Inception/CN=${DOMAIN_NAME}"
+
+chmod 600 ${CERTS_CRT} ${CERTS_KEY}
+
+sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|g" /etc/nginx/conf.d/nginx.conf
+sed -i "s|KEY_PATH|${CERTS_KEY}|g" /etc/nginx/conf.d/nginx.conf
+sed -i "s|CRT_PATH|${CERTS_CRT}|g" /etc/nginx/conf.d/nginx.conf
+
+echo "127.0.0.1 ${DOMAIN_NAME}" >> /etc/hosts
+
+exec "$@"
